400a68e34febb7fe701525c63fa56ecc03d28a0c
Mandy <drgnfli6420@yahoo.com>
2020-12-10 19:56:22 -0500

    loops favorite foods and movies

---
+++ script.js
@@ -0,0 +1,49 @@
+let moviesArray = ["It", "E.T.", "Up", "It 2", "D2", "Red", "Us", "Go"]
+
+// //while loops
+
+// console.log(moviesArray.length)
+
+// let i = 0
+
+// while(i < moviesArray.length){
+//     console.log(moviesArray[i])
+//     i++
+// }
+
+// let i = 1
+
+// while(i < 20){
+//     console.log("Hello, World!")
+//     i++
+// }
+// Psychotic Bird
+// In your JavaScript file, write a while loop that outputs the phrase "TWEET TWEET" 100 times to the console.
+
+// let i = 1
+
+// while(i <= 100){
+//     console.log("Tweet Tweet")
+//     i++
+// }
+
+for(let i = 0; i < 100; i++){
+    console.log("Tweet Tweet Tweet")
+}
+
+//For Loops
+
+for(let i = 0; i < moviesArray.length; i++){
+    console.log(moviesArray[i])
+}
+
+// Favorite Foods
+// In your JavaScript, create a new variable called favoriteFoods. It should store an array of strings representing five of your favorite foods.
+// Loop through the array and print each food item to the console.
+
+let favoriteFoods = ["Pizza", "Salad", "Spaghetti", "Lasagna", "Candy"]
+
+for(let i = 0; i < favoriteFoods.length; i++){
+    console.log(favoriteFoods[i])
+}
+
